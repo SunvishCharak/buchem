ae64a76f2afbecd3ff8b5c6a66def97357a9a79b
Singla10 <tishasingla10@gmail.com>
2025-03-15 15:59:03 +0530

    shipping policy

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,8 @@ import PrivacyPolicy from "./Pages/PrivacyPolicy.js";
 import ScrollToTop from "./Components/scrolltotop.js";
 import ShopContextProvider from "./context/ShopContext.js";
 import TrackOrder from "./Pages/TrackOrder.js";
+import ResetPassword from "./Pages/ResetPassword.js";
+import ShippingPolicy from "./Pages/ShippingPolicy.js"
 
 const App = () => {
   return (
@@ -46,6 +48,8 @@ const App = () => {
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/track-order" element={<TrackOrder />} />
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path="/shipping-policy" element={<ShippingPolicy/>}/>
       </Routes>
       <WhatsApp />
       <Footer />
