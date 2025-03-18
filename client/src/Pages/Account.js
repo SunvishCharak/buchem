85438562579af26528d52a08f639b1643cce89e0
Singla10 <tishasingla10@gmail.com>
2025-03-18 13:09:44 +0530

    prodcts page

--- client/src/Pages/Account.js
+++ client/src/Pages/Account.js
@@ -8,10 +8,7 @@ const AccountPage = () => {
   const {
     userAccount,
     fetchUserAccount,
-    wishlist,
-    addToCart,
-    removeFromWishlist,
-    getUserWishlist,
+   
   } = useContext(ShopContext);
   const [activeSection, setActiveSection] = useState("details");
 
@@ -19,14 +16,14 @@ const AccountPage = () => {
     fetchUserAccount();
   }, [fetchUserAccount]);
 
-  const handleRemoveFromWishlist = async (itemId) => {
-    await removeFromWishlist(itemId);
-    getUserWishlist();
-  };
+  // const handleRemoveFromWishlist = async (itemId) => {
+  //   await removeFromWishlist(itemId);
+  //   getUserWishlist();
+  // };
 
-  useEffect(() => {
-    getUserWishlist();
-  }, []);
+  // useEffect(() => {
+  //   getUserWishlist();
+  // }, []);
 
   const token = localStorage.getItem("token");
 
@@ -41,45 +38,45 @@ const AccountPage = () => {
 
   const renderContent = () => {
     switch (activeSection) {
-      case "orders":
-        return (
-          <div className="orders container">
-            <Orders />
-          </div>
-        );
-      case "wishlist":
-        return (
-          <div className="wishlist container">
-            <h2>Your Wishlist</h2>
-            {wishlist?.length === 0 ? (
-              <p>Your wishlist is empty.</p>
-            ) : (
-              <div className="wishlist-container">
-                {wishlist
-                  ?.filter((item) => item && item._id)
-                  .map((item) => (
-                    <div key={item._id} className="wishlist-item">
-                      <ProductItem
-                        id={item._id}
-                        image={item.image}
-                        name={item.name}
-                        price={item.price}
-                        className="wishlist-product"
-                      />
-                      <button onClick={() => addToCart(item._id)}>
-                        Add to Cart
-                      </button>
-                      <button
-                        onClick={() => handleRemoveFromWishlist(item._id)}
-                      >
-                        Remove
-                      </button>
-                    </div>
-                  ))}
-              </div>
-            )}
-          </div>
-        );
+      // case "orders":
+      //   return (
+      //     <div className="orders container">
+      //       <Orders />
+      //     </div>
+      //   );
+      // case "wishlist":
+      //   return (
+      //     <div className="wishlist container">
+      //       <h2>Your Wishlist</h2>
+      //       {wishlist?.length === 0 ? (
+      //         <p>Your wishlist is empty.</p>
+      //       ) : (
+      //         <div className="wishlist-container">
+      //           {wishlist
+      //             ?.filter((item) => item && item._id)
+      //             .map((item) => (
+      //               <div key={item._id} className="wishlist-item">
+      //                 <ProductItem
+      //                   id={item._id}
+      //                   image={item.image}
+      //                   name={item.name}
+      //                   price={item.price}
+      //                   className="wishlist-product"
+      //                 />
+      //                 <button onClick={() => addToCart(item._id)}>
+              //           Add to Cart
+              //         </button>
+              //         <button
+              //           onClick={() => handleRemoveFromWishlist(item._id)}
+              //         >
+              //           Remove
+              //         </button>
+              //       </div>
+              //     ))}
+              // </div>
+        //     )}
+        //   </div>
+        // );
       case "wallet":
         return <p>Wallet Balance: ${userAccount.wallet}</p>;
       default:
@@ -99,8 +96,8 @@ const AccountPage = () => {
         <ul>
           {[
             { name: "Details", key: "details" },
-            { name: "Orders", key: "orders" },
-            { name: "Wishlist", key: "wishlist" },
+            // { name: "Orders", key: "orders" },
+            // { name: "Wishlist", key: "wishlist" },
             { name: "Wallet", key: "wallet" },
           ].map((item) => (
             <li key={item.key}>
